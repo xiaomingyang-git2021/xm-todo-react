fa3866ffab102996590bb6bb235364a0c9f53c0b
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-03-05 17:45:11 -0500

    modified a file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
 
 function App(props) {
-  const taskList = props.tasks?.map(task =>
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>);
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks.map(task => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id}/>)
+    );
 
   function addTask(name) {
     alert(name);
