5b14495bbca7b79a321bcc37c64b6e6fe27d0e4d
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-03-05 18:22:45 -0500

    modified a file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
@@ -10,7 +11,8 @@ function App(props) {
     );
 
   function addTask(name) {
-    alert(name);
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
   return (
     <div className='todoapp stack-large'>
